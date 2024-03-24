cb89a39811871746bcfb8b5c43d8f6ba7a35388a
s28369 <s28369@pjwstk.edu.pl>
2024-03-24 12:07:33 +0100

    test

--- ConsoleApp1/ConsoleApp1/Project.cs
+++ ConsoleApp1/ConsoleApp1/Project.cs
@@ -8,10 +8,16 @@ public class Project
 {
     public static void Main(string[] args)
     {
-        KontenerPlyn kont = new KontenerPlyn(100, 100, 100, 10, "hazard");
-        kont.Zaladowac(99);
-        KontenerPlyn kont1 = new KontenerPlyn(100, 100, 100, 10, "normal");
-        kont1.Zaladowac(100);
+        KontenerPlyn Kont = new KontenerPlyn(100, 100, 100, 10, "hazard");
+        Kont.Zaladowac(99);
+        KontenerPlyn Kont1 = new KontenerPlyn(100, 100, 100, 10, "normal");
+        Kont1.Zaladowac(100);
+        KontenerGaz KontG = new KontenerGaz(150, 150, 150, 50, 20);
+        KontG.Zaladowac(100);
+        KontG.Zaladowac(150);
+        Console.WriteLine("Cool");
+        Dictionary<string, double> ProduktyCold = new Dictionary<string, double>();
         
+
     }
 }
